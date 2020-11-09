f217f8f95aa8f25fe22451e7ce1bed80d8b6c24d
trungnus96 <trungnus96@gmail.com>
2020-11-09 16:16:35 +1100

    Debug

--- next.config.js
+++ next.config.js
@@ -2,7 +2,7 @@ const withPlugins = require("next-compose-plugins");
 const sass = require("@zeit/next-sass");
 const css = require("@zeit/next-css");
 
-module.exports = withPlugins([[sass, css]], {
+module.exports = withPlugins([], {
   // next config here
   // Target must be serverless
   target: "serverless",
